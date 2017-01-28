30110741877e6286116a41dadf831ce067f20b58
Teodor Karabeliov <Teodor Karabeliov>
2017-01-28 12:14:45 +0200

    Initial

---
+++ HelloJenkins.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloJenkins.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using HelloJenkins;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello Jenkins", Program.CreateMessage());
+        }
+    }
+}
